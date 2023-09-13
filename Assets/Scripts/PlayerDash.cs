9bbd062e2be4e3201c1adec6e0d763b134f19aba
Linus <97689021+D9Linus@users.noreply.github.com>
2023-09-13 19:08:44 +0200

    PlayerDash
    Fungerande PlayerDash Script (Behövs optimeras

---
+++ Assets/Scripts/PlayerDash.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+    [SerializeField] private float dashCooldown;
+
+    public float dashForce = 10f;
+    public float dashDuration = 0.2f;
+    private bool isDashing = false;
+
+    void Update()
+    {
+
+
+        if ((Input.GetKeyDown(KeyCode.LeftShift) && !isDashing) || (Input.GetKeyDown(KeyCode.RightShift) && !isDashing))
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        isDashing = true;
+
+        // Spelarens ingångshastighet
+        Vector2 originalVelocity = GetComponent<Rigidbody2D>().velocity;
+
+        // Standard dash åt höger
+        Vector2 dashDirection = transform.right;
+
+        // Ändrad dash åt vänster
+        if (GetComponent<PlayerMovement>().playerDirection == 2)
+        {
+            dashDirection = -dashDirection;
+        }
+
+        // Totalkraft efter dash
+        GetComponent<Rigidbody2D>().AddForce(dashDirection * dashForce, ForceMode2D.Impulse);
+
+        // Förhindrar annan rörelse under dashen (dashen funkar inte utan detta??)
+        GetComponent<PlayerMovement>().enabled = false;
+
+        // Dash längden (ändras i Unity under Player i Script)
+        yield return new WaitForSeconds(dashDuration);
+
+        // Återställ 
+        GetComponent<Rigidbody2D>().velocity = originalVelocity;
+        GetComponent<PlayerMovement>().enabled = true;
+
+        // Gör så att spelaren får en cooldown innan den kan använda dashen igen
+        yield return new WaitForSeconds(dashCooldown);
+        isDashing = false;
+    }
+}
